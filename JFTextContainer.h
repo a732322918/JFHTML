84eee8aa4608a58978a5d00d4c051d33535067e1
a732322918 <fu980210@gmail.com>
2016-05-16 15:02:14 +0800

    移动JFTextKit

---
+++ JFTextContainer.h
@@ -0,0 +1,13 @@
+//
+//  JFTextContainer.h
+//  JFHTML
+//
+//  Created by 付书炯 on 16/5/6.
+//  Copyright © 2016年 shujiong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JFTextContainer : NSObject
+
+@end
