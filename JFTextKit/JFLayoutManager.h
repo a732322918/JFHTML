c36cfa42189f5e384aa7a753eeb27b74ee4a63ed
a732322918 <fu980210@gmail.com>
2016-05-16 15:02:14 +0800

    移动JFTextKit

---
+++ JFTextKit/JFLayoutManager.h
@@ -0,0 +1,13 @@
+//
+//  JFLayoutManager.h
+//  JFHTML
+//
+//  Created by 付书炯 on 16/5/6.
+//  Copyright © 2016年 shujiong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JFLayoutManager : NSObject
+
+@end
---
+++ JFTextKit/JFTextContainer.m
@@ -0,0 +1,13 @@
+//
+//  JFTextContainer.m
+//  JFHTML
+//
+//  Created by 付书炯 on 16/5/6.
+//  Copyright © 2016年 shujiong. All rights reserved.
+//
+
+#import "JFTextContainer.h"
+
+@implementation JFTextContainer
+
+@end
